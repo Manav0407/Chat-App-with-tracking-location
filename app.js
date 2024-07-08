4fb4c053434e3edc327d5ca64551d58af57dabf3
Manav0407 <manavckantharia@gmail.com>
2024-07-09 02:39:57 +0530

    mhh

--- app.js
+++ app.js
@@ -194,6 +194,13 @@ app.use(bodyParser.json({ limit: "10mb" }));
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 // Connect to the database
 connectDB();
 
